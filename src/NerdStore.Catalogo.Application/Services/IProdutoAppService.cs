a42a0544ca9acec35ae98f0f6544fee105cd5ca8
Carlos Eduardo Lima Rocha <edulima2412@gmail.com>
2023-06-28 15:25:20 -0300

    first commit

---
+++ src/NerdStore.Catalogo.Application/Services/IProdutoAppService.cs
@@ -0,0 +1,18 @@
+﻿using NerdStore.Catalogo.Application.ViewModels;
+
+namespace NerdStore.Catalogo.Application.Services
+{
+    public interface IProdutoAppService : IDisposable
+    {
+        Task<IEnumerable<ProdutoViewModel>> ObterTodos();
+        Task<ProdutoViewModel> ObterPorId(Guid id);
+        Task<IEnumerable<ProdutoViewModel>> ObterPorCategoria(int codigo);
+        Task<IEnumerable<CategoriaViewModel>> ObterCategorias();
+
+        Task AdicionarProduto(ProdutoViewModel produtoViewModel);
+        Task AtualizarProduto(ProdutoViewModel produtoViewModel);
+
+        Task<ProdutoViewModel> DebitarEstoque(Guid produtoId, int quantidade);
+        Task<ProdutoViewModel> ReporEstoque(Guid produtoId, int quantidade);
+    }
+}
---
+++ src/NerdStore.Catalogo.Data/Mappings/ProdutoMapping.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NerdStore.Catalogo.Domain;
+
+namespace NerdStore.Catalogo.Data.Mappings
+{
+    public class ProdutoMapping : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(250)");
+
+            builder.Property(c => c.Descricao)
+                .IsRequired()
+                .HasColumnType("varchar(500)");
+
+            builder.Property(c => c.Imagem)
+                .IsRequired()
+                .HasColumnType("varchar(250)");
+
+            // Transforma VO em colunas
+            builder.OwnsOne(c => c.Dimensoes, cm =>
+            {
+                cm.Property(c => c.Altura)
+                    .HasColumnName("Altura")
+                    .HasColumnType("decimal(10,2)");
+
+                cm.Property(c => c.Largura)
+                    .HasColumnName("Largura")
+                    .HasColumnType("decimal(10,2)");
+
+                cm.Property(c => c.Profundidade)
+                    .HasColumnName("Profundidade")
+                    .HasColumnType("decimal(10,2)");
+            });
+
+            builder.ToTable("Produtos");
+        }
+    }
+}
